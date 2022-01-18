1ed0215aac3ca9f0fce579779e05a72eee306adf
Dima <dimader@bk.ru>
2022-01-19 01:41:17 +0700

    Добавлена форма InterviewForm для прохождения опроса

---
+++ InterviewForm.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace derveninov01_kursovaya_prog3
+{
+    partial class InterviewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InterviewForm));
+            this.pnMain = new System.Windows.Forms.FlowLayoutPanel();
+            this.btNext = new System.Windows.Forms.Button();
+            this.btEnd = new System.Windows.Forms.Button();
+            this.pnMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.Controls.Add(this.btNext);
+            this.pnMain.Controls.Add(this.btEnd);
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(405, 344);
+            this.pnMain.TabIndex = 0;
+            // 
+            // btNext
+            // 
+            this.btNext.Location = new System.Drawing.Point(3, 3);
+            this.btNext.Name = "btNext";
+            this.btNext.Size = new System.Drawing.Size(139, 29);
+            this.btNext.TabIndex = 0;
+            this.btNext.Text = "Следующий вопрос";
+            this.btNext.UseVisualStyleBackColor = true;
+            this.btNext.Click += new System.EventHandler(this.btNext_Click);
+            // 
+            // btEnd
+            // 
+            this.btEnd.Location = new System.Drawing.Point(148, 3);
+            this.btEnd.Name = "btEnd";
+            this.btEnd.Size = new System.Drawing.Size(139, 29);
+            this.btEnd.TabIndex = 1;
+            this.btEnd.Text = "Завершить опрос";
+            this.btEnd.UseVisualStyleBackColor = true;
+            this.btEnd.Click += new System.EventHandler(this.btEnd_Click);
+            // 
+            // InterviewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 344);
+            this.Controls.Add(this.pnMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "InterviewForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Опросник";
+            this.Load += new System.EventHandler(this.InterviewForm_OnLoad);
+            this.pnMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel pnMain;
+        private System.Windows.Forms.Button btNext;
+        private System.Windows.Forms.Button btEnd;
+    }
+}
---
+++ InterviewForm.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace derveninov01_kursovaya_prog3
+{
+    public partial class InterviewForm : Form
+    {
+        // Процесс прохождения опроса
+        private Interview interview;
+        // Текущий опросник
+        private Questionnaire questionnaire;
+        // Заполненный опросник
+        private FilledQuestionnaire filledQuestionnaire;
+        public InterviewForm()
+        {
+            InitializeComponent();
+        }
+        public InterviewForm(Questionnaire questionnaire)
+        {
+            InitializeComponent();
+            this.questionnaire = questionnaire;
+        }
+
+        private void InterviewForm_OnLoad(object sender, EventArgs e)
+        {
+            filledQuestionnaire = new FilledQuestionnaire();
+            if (questionnaire == null)
+            {
+                var ofd = new OpenFileDialog() { Filter = "Опросник|*.q", Title = "Выберите опросник" };
+                if (ofd.ShowDialog(this) == DialogResult.OK)
+                {
+                    questionnaire = FileHelper.LoadQuest<Questionnaire>(ofd.FileName);
+                }
+                else
+                {
+                    
+                    Close();
+                    return;
+                }
+            }
+            foreach (Question question in questionnaire)
+            {
+                Answer answer = new Answer() { QuestionTitle = question.Title };
+                filledQuestionnaire.Add(answer);
+            }
+            interview = new Interview(questionnaire, filledQuestionnaire);
+            Build();
+        }
+        /// <summary>
+        /// Построить интерфейс опросника
+        /// </summary>
+        public void Build()
+        {
+            // Очистить панель ответов
+            pnMain.Controls.Clear();
+            // Отобразить отвеченные вопросы
+            for (int i = 0; i < interview.CurrentQuestionIndex; i++)
+            {
+                // Создать панель ответа
+                var pn = new AnswerPannel();
+                // Построить панель ответа
+                pn.Build(interview, questionnaire[i], filledQuestionnaire[i], true);
+                // Добавить панель на форму
+                pn.Parent = pnMain;
+            }
+            // Отобразить текущий вопрос, на который не дан ответ
+            if (interview.CurrentQuestionIndex < questionnaire.Count)
+            {
+                // Создать панель ответа
+                var pn = new AnswerPannel();
+                // Построить панель ответа
+                pn.Build(interview, questionnaire[interview.CurrentQuestionIndex], filledQuestionnaire[interview.CurrentQuestionIndex], false);
+                // Добавить панель на форму
+                pn.Parent = pnMain;
+            }
+            // Отображение кнопок "След. вопрос" и "Завершить опрос"
+            if (interview.IsFinished) btEnd.Parent = pnMain;
+            else btNext.Parent = pnMain;
+        }
+        /// <summary>
+        /// Переход к следующему вопросу
+        /// </summary>
+        private void btNext_Click(object sender, EventArgs e)
+        {
+            interview.NextQuestion();
+            Build();
+        }
+        /// <summary>
+        /// Завершить опрос
+        /// </summary>
+        private void btEnd_Click(object sender, EventArgs e)
+        {
+            // Сохранение заполненного опросника
+            if (MessageBox.Show("Сохранить результат опроса?", "Сохранение результата опроса", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                var sfd = new SaveFileDialog() { Filter = "Результат опроса | *.txt", FileName = Guid.NewGuid().ToString() };
+                if (sfd.ShowDialog(this) == DialogResult.OK)
+                {
+                    TextWriter tw = new StreamWriter(sfd.FileName);
+                    foreach (Answer item in filledQuestionnaire)
+                    {
+                        tw.WriteLine(item.ToString());
+                    }
+                    tw.Close();
+                }
+            }
+            Close();
+        }
+    }
+}
