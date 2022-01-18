6162bd6f7d983609ecb01cd8f261b4a07d977dff
Dima <dimader@bk.ru>
2022-01-19 01:38:06 +0700

    Добавлен класс FilledQuestionnaire (Заполненный опросник)

---
+++ FilledQuestionnaire.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace derveninov01_kursovaya_prog3
+{
+    /// <summary>
+    /// Заполненный опросник,
+    /// Состоит из списка ответов
+    /// </summary>
+    public class FilledQuestionnaire : List<Answer>
+    {
+    }
+    /// <summary>
+    /// Ответ опрашиваемого на вопрос
+    /// </summary>
+    public class Answer
+    {
+        /// <summary>
+        /// Текст ответа на вопрос
+        /// </summary>
+        public string AnswerText;
+
+        /// <summary>
+        /// Текст вопроса
+        /// </summary>
+        public string QuestionTitle;
+
+        public override string ToString()
+        {
+            string s = QuestionTitle + ": " + AnswerText;
+            return s;
+        }
+    }
+}
