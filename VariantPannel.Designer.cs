b04913445bac7b94da749ee17b4183c70858c7da
Dima <dimader@bk.ru>
2022-01-19 01:37:13 +0700

    Добавлен пользовательский элемент управления VariantPannel для добавления вариантов ответов к вопросу

---
+++ VariantPannel.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace derveninov01_kursovaya_prog3
+{
+    partial class VariantPannel
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VariantPannel));
+            this.tbTitle = new System.Windows.Forms.TextBox();
+            this.btDelete = new System.Windows.Forms.Button();
+            this.btDown = new System.Windows.Forms.Button();
+            this.btUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbTitle
+            // 
+            this.tbTitle.Location = new System.Drawing.Point(5, 3);
+            this.tbTitle.Name = "tbTitle";
+            this.tbTitle.Size = new System.Drawing.Size(291, 23);
+            this.tbTitle.TabIndex = 1;
+            this.tbTitle.TextChanged += new System.EventHandler(this.tbTitle_TextChanged);
+            // 
+            // btDelete
+            // 
+            this.btDelete.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btDelete.BackgroundImage")));
+            this.btDelete.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btDelete.Location = new System.Drawing.Point(365, 3);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(25, 23);
+            this.btDelete.TabIndex = 2;
+            this.btDelete.UseVisualStyleBackColor = true;
+            this.btDelete.Click += new System.EventHandler(this.btDelete_Click);
+            // 
+            // btDown
+            // 
+            this.btDown.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btDown.BackgroundImage")));
+            this.btDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btDown.Location = new System.Drawing.Point(334, 3);
+            this.btDown.Name = "btDown";
+            this.btDown.Size = new System.Drawing.Size(25, 23);
+            this.btDown.TabIndex = 3;
+            this.btDown.UseVisualStyleBackColor = true;
+            this.btDown.Click += new System.EventHandler(this.btDown_Click);
+            // 
+            // btUp
+            // 
+            this.btUp.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btUp.BackgroundImage")));
+            this.btUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btUp.Location = new System.Drawing.Point(303, 3);
+            this.btUp.Name = "btUp";
+            this.btUp.Size = new System.Drawing.Size(25, 23);
+            this.btUp.TabIndex = 4;
+            this.btUp.UseVisualStyleBackColor = true;
+            this.btUp.Click += new System.EventHandler(this.btUp_Click);
+            // 
+            // VariantPannel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.btUp);
+            this.Controls.Add(this.btDown);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.tbTitle);
+            this.Name = "VariantPannel";
+            this.Size = new System.Drawing.Size(393, 30);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbTitle;
+        private System.Windows.Forms.Button btDelete;
+        private System.Windows.Forms.Button btDown;
+        private System.Windows.Forms.Button btUp;
+    }
+}
---
+++ VariantPannel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace derveninov01_kursovaya_prog3
+{
+    public partial class VariantPannel : UserControl
+    {
+        // Текущий вопрос
+        private Question question;
+        // Текущий вариант ответа
+        private Variant variant;
+        // Событие "Вариант изменился"
+        public event Action VariantChanged;
+        public VariantPannel()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Построить интерфейс варианта ответа
+        /// </summary>
+        public void Build(Question question, Variant variant)
+        {
+            this.question = question;
+            this.variant = variant;
+
+            tbTitle.Text = variant.Title;
+        }
+        /// <summary>
+        /// Передвинуть вариант ответа вверх
+        /// </summary>
+        private void btUp_Click(object sender, EventArgs e)
+        {
+            question.MoveVariant(question, variant, -1);
+            VariantChanged();
+        }
+        /// <summary>
+        /// Передвинуть вариант ответа ввниз
+        /// </summary>
+        private void btDown_Click(object sender, EventArgs e)
+        {
+            question.MoveVariant(question, variant, 1);
+            VariantChanged();
+        }
+        /// <summary>
+        /// Удаление варианта ответа
+        /// </summary>
+        private void btDelete_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Удалить вариант ответа?", "Удаление варианта ответа", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                //удаляем вопрос
+                question.Remove(variant);
+                //сигнализируем наверх о том, что список вопросов поменялся
+                VariantChanged();
+            }
+        }
+
+        private void tbTitle_TextChanged(object sender, EventArgs e)
+        {
+            variant.Title = tbTitle.Text;
+        }
+    }
+}
