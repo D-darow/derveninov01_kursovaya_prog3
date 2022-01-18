b0ef4fadeecfcd6d8cdd03adf8ec5a3ab6b183a9
Dima <dimader@bk.ru>
2022-01-19 01:42:21 +0700

    Добавлен пользовательский элемент управления AnswerPannel для отображения вопросов

---
+++ AnswerPannel.Designer.cs
@@ -0,0 +1,69 @@
+﻿
+namespace derveninov01_kursovaya_prog3
+{
+    partial class AnswerPannel
+    {
+        /// <summary> 
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary> 
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbQuestionTitle = new System.Windows.Forms.Label();
+            this.pnMain = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // lbQuestionTitle
+            // 
+            this.lbQuestionTitle.Location = new System.Drawing.Point(3, 0);
+            this.lbQuestionTitle.Name = "lbQuestionTitle";
+            this.lbQuestionTitle.Size = new System.Drawing.Size(322, 33);
+            this.lbQuestionTitle.TabIndex = 0;
+            this.lbQuestionTitle.Text = "Текст вопроса";
+            // 
+            // pnMain
+            // 
+            this.pnMain.Location = new System.Drawing.Point(3, 36);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(322, 33);
+            this.pnMain.TabIndex = 1;
+            // 
+            // AnswerPannel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.pnMain);
+            this.Controls.Add(this.lbQuestionTitle);
+            this.Name = "AnswerPannel";
+            this.Size = new System.Drawing.Size(326, 75);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbQuestionTitle;
+        private System.Windows.Forms.FlowLayoutPanel pnMain;
+    }
+}
---
+++ AnswerPannel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace derveninov01_kursovaya_prog3
+{
+    public partial class AnswerPannel : UserControl
+    {
+        // Текущий вопрос
+        private Question question;
+        // Текущий вопрос
+        private Answer answer;
+        // Текущий процесс прохождения опроса
+        private Interview interview;
+        public AnswerPannel()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Построить интерфейс
+        /// </summary>
+        public void Build(Interview interview, Question question, Answer answer, bool passed)
+        {
+            this.question = question;
+            this.answer = answer;
+            this.interview = interview;
+            lbQuestionTitle.Text = question.Title;
+            
+            if (passed) // Если вопрос отвечен
+            {
+                var lb = new Label();
+                lb.Text = answer.AnswerText;
+                lb.Parent = pnMain;
+            }
+            else
+            {
+                switch (question.questionType)
+                {
+                    case QuestionType.FreeAnswer:
+                        // Создание TextBox элемента
+                        var tb = new TextBox();
+                        tb.Parent = pnMain;
+                        // Добавить событие "Текст изменен"
+                        tb.TextChanged += (o, O) =>
+                        {
+                            answer.AnswerText = tb.Text;
+                        };
+                        break;
+                    case QuestionType.SingleAnswer:
+                        // Создание ComboBox элемента
+                        var cb = new ComboBox();
+                        List<string> variants = new List<string>();
+                        foreach (Variant variant in question)
+                        {
+                            variants.Add(variant.Title);
+                        }
+                        cb.DataSource = variants;
+                        cb.DropDownStyle = ComboBoxStyle.DropDownList;
+                        cb.Parent = pnMain;
+                        // Добавить событие "Изменен выбор ComboBox"
+                        cb.SelectedValueChanged += (o, O) =>
+                        {
+                            answer.AnswerText = cb.SelectedValue.ToString();
+                        };
+                        if (variants.Count > 0) answer.AnswerText = variants[0];
+                        break;
+                }
+            }
+        }
+    }
+}
