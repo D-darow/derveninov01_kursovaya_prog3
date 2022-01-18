fc6012585a109392083c6db1b5168811922bf282
Dima <dimader@bk.ru>
2022-01-19 01:34:43 +0700

    Главная форма

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,104 @@ namespace derveninov01_kursovaya_prog3
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainToolStrip = new System.Windows.Forms.ToolStrip();
+            this.btnOpenQuest = new System.Windows.Forms.ToolStripButton();
+            this.btnSaveQuest = new System.Windows.Forms.ToolStripButton();
+            this.btnAddQuest = new System.Windows.Forms.ToolStripButton();
+            this.btnStartQuest = new System.Windows.Forms.ToolStripButton();
+            this.MainPannel = new System.Windows.Forms.FlowLayoutPanel();
+            this.MainToolStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainToolStrip
+            // 
+            this.MainToolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpenQuest,
+            this.btnSaveQuest,
+            this.btnAddQuest,
+            this.btnStartQuest});
+            this.MainToolStrip.Location = new System.Drawing.Point(0, 0);
+            this.MainToolStrip.Name = "MainToolStrip";
+            this.MainToolStrip.Size = new System.Drawing.Size(455, 25);
+            this.MainToolStrip.TabIndex = 0;
+            this.MainToolStrip.Text = "toolStrip1";
+            // 
+            // btnOpenQuest
+            // 
+            this.btnOpenQuest.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnOpenQuest.Image = ((System.Drawing.Image)(resources.GetObject("btnOpenQuest.Image")));
+            this.btnOpenQuest.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpenQuest.Name = "btnOpenQuest";
+            this.btnOpenQuest.Size = new System.Drawing.Size(23, 22);
+            this.btnOpenQuest.Text = "Открыть опросник";
+            this.btnOpenQuest.Click += new System.EventHandler(this.btnOpenQuest_Click);
+            // 
+            // btnSaveQuest
+            // 
+            this.btnSaveQuest.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSaveQuest.Image = ((System.Drawing.Image)(resources.GetObject("btnSaveQuest.Image")));
+            this.btnSaveQuest.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSaveQuest.Name = "btnSaveQuest";
+            this.btnSaveQuest.Size = new System.Drawing.Size(23, 22);
+            this.btnSaveQuest.Text = "Сохранить опросник";
+            this.btnSaveQuest.Click += new System.EventHandler(this.btnSaveQuest_Click);
+            // 
+            // btnAddQuest
+            // 
+            this.btnAddQuest.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnAddQuest.Image = ((System.Drawing.Image)(resources.GetObject("btnAddQuest.Image")));
+            this.btnAddQuest.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAddQuest.Name = "btnAddQuest";
+            this.btnAddQuest.Size = new System.Drawing.Size(23, 22);
+            this.btnAddQuest.Text = "Добавить вопрос";
+            this.btnAddQuest.Click += new System.EventHandler(this.btnAddQuest_Click);
+            // 
+            // btnStartQuest
+            // 
+            this.btnStartQuest.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnStartQuest.Image = ((System.Drawing.Image)(resources.GetObject("btnStartQuest.Image")));
+            this.btnStartQuest.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStartQuest.Name = "btnStartQuest";
+            this.btnStartQuest.Size = new System.Drawing.Size(23, 22);
+            this.btnStartQuest.Text = "Начать опрос";
+            this.btnStartQuest.Click += new System.EventHandler(this.btnStartQuest_Click);
+            // 
+            // MainPannel
+            // 
+            this.MainPannel.AutoScroll = true;
+            this.MainPannel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainPannel.Location = new System.Drawing.Point(0, 25);
+            this.MainPannel.Name = "MainPannel";
+            this.MainPannel.Size = new System.Drawing.Size(455, 425);
+            this.MainPannel.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(455, 450);
+            this.Controls.Add(this.MainPannel);
+            this.Controls.Add(this.MainToolStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Опрос пациента";
+            this.MainToolStrip.ResumeLayout(false);
+            this.MainToolStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip MainToolStrip;
+        private System.Windows.Forms.ToolStripButton btnOpenQuest;
+        private System.Windows.Forms.ToolStripButton btnSaveQuest;
+        private System.Windows.Forms.ToolStripButton btnAddQuest;
+        private System.Windows.Forms.ToolStripButton btnStartQuest;
+        private System.Windows.Forms.FlowLayoutPanel MainPannel;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,10 +12,87 @@ namespace derveninov01_kursovaya_prog3
 {
     public partial class Form1 : Form
     {
+        // Текущий опросник
+        private Questionnaire questionnaire = new Questionnaire();
+
         public Form1()
         {
             InitializeComponent();
         }
-
+        /// <summary>
+        /// Построить интерфейс
+        /// </summary>
+        private void Build()
+        {
+            // Очистить главную панель
+            MainPannel.Controls.Clear();
+            // Создать панели для каждого вопроса
+            foreach (Question item in questionnaire)
+            {
+                QuestionPannel pn = new QuestionPannel();
+                pn.Build(questionnaire, item);
+                pn.QuestionnaireChanged += () =>
+                {
+                    Build();
+                };
+                MainPannel.Controls.Add(pn);
+            }
+            // Вызов сборщика мусора
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+        }
+        /// <summary>
+        /// Открыть готовый опросник
+        /// </summary>
+        private void btnOpenQuest_Click(object sender, EventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog() { Filter = "Опросник|*.quest" };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                questionnaire = FileHelper.LoadQuest<Questionnaire>(openFileDialog.FileName);
+                Build();
+            }
+        }
+        /// <summary>
+        /// Сохранить опросник
+        /// </summary>
+        private void btnSaveQuest_Click(object sender, EventArgs e)
+        {
+            if (questionnaire.Count == 0)
+            {
+                throw new Exception("Опросник пуст. Невозможно сохранить опросник...");
+            }
+            var saveFileDialog = new SaveFileDialog() { Filter = "Опросник|*.quest" };
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                FileHelper.SaveQuest(questionnaire, saveFileDialog.FileName);
+            }
+        }
+        /// <summary>
+        /// Добавить вопрос
+        /// </summary>
+        private void btnAddQuest_Click(object sender, EventArgs e)
+        {
+            questionnaire.AddQuestion(questionnaire);
+            Build();
+        }
+        /// <summary>
+        /// Начать опрос
+        /// </summary>
+        private void btnStartQuest_Click(object sender, EventArgs e)
+        {
+            if (questionnaire.Count == 0)
+            {
+                throw new Exception("Опросник пуст. Невозможно начать опрос...");
+            }
+            foreach (Question question in questionnaire)
+            {
+                if (question.Count == 0) 
+                {
+                    throw new Exception("Не во всех вопросах есть варианты ответа...");
+                }
+            }
+            new InterviewForm(questionnaire).ShowDialog();
+        }
     }
 }
